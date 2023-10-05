9cf55273331b031aec61cd481ea25acf1d2172d2
pjh5474 <warwarsn@gmail.com>
2023-10-06 02:17:05 +0900

    Router and @types

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useReactiveVar } from "@apollo/client";
-import { Outlet } from "react-router-dom";
 import { isLoggedInVar } from "./apollo";
-import { LoggedOut } from "./routes/logged-out";
+import { LoggedInRouter } from "./routers/logged-in-router";
+import { LoggedOutRouter } from "./routers/logged-out-router";
 
 function App() {
-  const isLoggedIn = useReactiveVar(isLoggedInVar);
-  return isLoggedIn ? <Outlet /> : <LoggedOut />;
+  return useReactiveVar(isLoggedInVar) ? (
+    <LoggedInRouter />
+  ) : (
+    <LoggedOutRouter />
+  );
 }
 
 export default App;
