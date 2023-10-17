ecdf34bc8a7e06e210abfb6f895bd64a01f03973
pjh5474 <warwarsn@gmail.com>
2023-10-17 15:26:16 +0900

    From Design & Login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,61 @@
+import { useForm } from "react-hook-form";
+
+interface ILoginForm {
+  email?: string;
+  password?: string;
+}
+
 export const Login = () => {
-  return <span>Login</span>;
+  const {
+    register,
+    getValues,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<ILoginForm>();
+  const onSubmit = () => {};
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-800">
+      <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
+        <h3 className="text-2xl text-gray-800">Log In</h3>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="grid gap-3 mt-5 px-5"
+        >
+          <input
+            {...register("email", {
+              required: "Email is required",
+            })}
+            name="email"
+            required
+            placeholder="Email"
+            className="input mb-3"
+          />
+          {errors.email?.message && (
+            <span className="font-medium text-red-500">
+              {errors.email?.message}
+            </span>
+          )}
+          <input
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 2,
+                message: "Password must be more than 2 chars.",
+              },
+            })}
+            name="password"
+            required
+            placeholder="Password"
+            className="input"
+          />
+          {errors.password?.message && (
+            <span className="font-medium text-red-500">
+              {errors.password?.message}
+            </span>
+          )}
+          <button className="mt-3 btn">Log In</button>
+        </form>
+      </div>
+    </div>
+  );
 };
