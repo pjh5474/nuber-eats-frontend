ff1ded6429c8c063144df5b5c6f128aafb206beb
pjh5474 <warwarsn@gmail.com>
2023-10-17 18:28:33 +0900

    UI Clonning

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,6 +2,9 @@ import { ApolloError, gql, useMutation } from "@apollo/client";
 import { useForm } from "react-hook-form";
 import { FormError } from "../components/form-error";
 import { LoginMutation, LoginMutationVariables } from "../__api__/types";
+import nuberLogo from "../images/logo.svg";
+import { Button } from "../components/button";
+import { Link } from "react-router-dom";
 
 const LOGIN_MUTATION = gql`
   mutation Login($loginInput: LoginInput!) {
@@ -22,9 +25,11 @@ export const Login = () => {
   const {
     register,
     getValues,
-    formState: { errors },
+    formState: { errors, isValid },
     handleSubmit,
-  } = useForm<ILoginForm>();
+  } = useForm<ILoginForm>({
+    mode: "onChange",
+  });
 
   const onCompleted = (data: LoginMutation) => {
     const {
@@ -58,12 +63,15 @@ export const Login = () => {
     }
   };
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-800">
-      <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
-        <h3 className="text-2xl text-gray-800">Log In</h3>
+    <div className="h-screen flex  items-center flex-col mt-10 lg:mt-28">
+      <div className="w-full max-w-screen-sm flex flex-col items-center px-5">
+        <img src={nuberLogo} className="w-52 mb-10" />
+        <h4 className="w-full font-medium text-left text-3xl mb-10">
+          Welcome Back
+        </h4>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="grid gap-3 mt-5 px-5"
+          className="grid gap-3 mt-5 w-full mb-5"
         >
           <input
             {...register("email", {
@@ -72,7 +80,7 @@ export const Login = () => {
             name="email"
             required
             placeholder="Email"
-            className="input mb-3"
+            className="input"
           />
           {errors.email?.message && (
             <FormError errorMessage={errors.email?.message} />
@@ -93,13 +101,20 @@ export const Login = () => {
           {errors.password?.message && (
             <FormError errorMessage={errors.password?.message} />
           )}
-          <button className="mt-3 btn">
-            {loading ? "Loading..." : "Log In"}
-          </button>
+          <Button canClick={isValid} loading={loading} actionText={"Log In"} />
           {loginMutationResult?.login.error && (
             <FormError errorMessage={loginMutationResult.login.error} />
           )}
         </form>
+        <div>
+          New to Nuber?{" "}
+          <Link
+            to="/create-account"
+            className=" text-lime-600 hover:underline "
+          >
+            Create an Account
+          </Link>
+        </div>
       </div>
     </div>
   );
